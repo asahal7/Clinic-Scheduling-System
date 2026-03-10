e86b6fa505f2c2d5a11c00132971c51b34329350
Abdimaalik Sahal <abdimaaaalik0@gmail.com>
2026-03-10 19:26:30 +0000

    changed location of clinicscheduling file to maintain clarity of the scope of each folder

--- src/main/java/com/abdimaalik/clinic/ClinicSchedulingApplication.java
+++ src/main/java/com/abdimaalik/clinic/ClinicSchedulingApplication.java
@@ -1,4 +1,4 @@
-package com.abdimaalik.clinic.domain;
+package com.abdimaalik.clinic
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
